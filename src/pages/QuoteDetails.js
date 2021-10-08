32785f0fea6c545503b5d12f22f839fffe1752bc
Alam <alam.15@outlook.com>
2021-10-08 15:26:09 -0400

    show quote in detail

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -2,14 +2,20 @@ import { useParams, Route } from "react-router-dom";
 import { Fragment } from "react";
 
 import Comments from "../components/comments/Comments";
+import { DUMMY_QUOTES } from "./Quotes";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 function QuoteDetails() {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) return <p>Quote Not Found</p>;
+
   return (
     <Fragment>
       <section>
-        <h1>Quote Details</h1>
-        <p>ID: {params.quoteId}</p>
+        <HighlightedQuote text={quote.text} author={quote.author} />
       </section>
       <Route path="/quotes/:quoteId/comments">
         <Comments />
--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,15 +1,21 @@
 import QuoteList from "../components/quotes/QuoteList";
 
-const DUMMY_QUOTES = [
+export const DUMMY_QUOTES = [
   {
+    id: "q1",
     text: "She's wild, but never easy, cool, but full of fire",
     author: "Jonny OX",
   },
   {
+    id: "q2",
     text: "I Knew I really love her, when I miss her, and she was still in my arms",
     author: "Atticus",
   },
-  { text: "Everything mortal has moments inmortal", author: "Amy Lowell" },
+  {
+    id: "q3",
+    text: "Everything mortal has moments inmortal",
+    author: "Amy Lowell",
+  },
 ];
 
 function Quotes() {
