133eb3fb8f2d13d23b5ea6ac0c5d7c9e7c61fac9
Alam <alam.15@outlook.com>
2021-10-08 13:55:10 -0400

    define routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route, Redirect, Switch } from "react-router-dom";
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          All Quotes
+        </Route>
+        <Route path="/quotes/new-quote">New Quote</Route>
+        <Route path="/quotes/:quoteId">Quote Details</Route>
+      </Switch>
     </div>
   );
 }
