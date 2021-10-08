0754aca2e7cf9cba414f47144499bab9a935a5a2
Alam <alam.15@outlook.com>
2021-10-08 15:04:10 -0400

    dummy quote list

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,7 +1,21 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    text: "She's wild, but never easy, cool, but full of fire",
+    author: "Jonny OX",
+  },
+  {
+    text: "I Knew I really love her, when I miss her, and she was still in my arms",
+    author: "Atticus",
+  },
+  { text: "Everything mortal has moments inmortal", author: "Amy Lowell" },
+];
+
 function Quotes() {
   return (
     <section>
-      <h1>Quotes</h1>
+      <QuoteList quotes={DUMMY_QUOTES} />
     </section>
   );
 }
