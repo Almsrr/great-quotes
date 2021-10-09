6c1652670423b37204aa6b9f9b438715a15e4eed
Alam <alam.15@outlook.com>
2021-10-09 17:35:12 -0400

    add comments features

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,26 +1,44 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
-import classes from './NewCommentForm.module.css';
+import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = (props) => {
   const commentTextRef = useRef();
+  const { sendRequest, status, error } = useHttp(addComment);
+  const { onAddedComment } = props;
 
   const submitFormHandler = (event) => {
     event.preventDefault();
 
-    // optional: Could validate here
-
-    // send comment to server
+    const commentText = commentTextRef.current.value;
+    sendRequest({ quoteId: props.quoteId, commentData: { text: commentText } });
   };
+
+  useEffect(() => {
+    if (status === "completed" && !error) {
+      onAddedComment();
+    }
+  }, [status, error, onAddedComment]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
 
   return (
     <form className={classes.form} onSubmit={submitFormHandler}>
       <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
+        <label htmlFor="comment">Your Comment</label>
+        <textarea id="comment" rows="5" ref={commentTextRef}></textarea>
       </div>
       <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
+        <button className="btn">Add Comment</button>
       </div>
     </form>
   );
